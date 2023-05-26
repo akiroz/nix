a9922f6af8cc1bacb5b820bcd67f060ecc89ad1d
akiroz <akiroz.vectis@gmail.com>
2023-05-27 04:24:52 +0800

    implement oci digest & manifest

--- src/libstore/oci-binary-cache-store.cc
+++ src/libstore/oci-binary-cache-store.cc
@@ -3,6 +3,9 @@
 #include "globals.hh"
 #include "nar-info-disk-cache.hh"
 #include "callback.hh"
+#include "hash.hh"
+
+#include <nlohmann/json.hpp>
 
 namespace nix {
 
@@ -22,45 +25,82 @@ struct OciBinaryCacheStoreConfig : virtual BinaryCacheStoreConfig
 
 struct OciBinaryCacheStore : public virtual OciBinaryCacheStoreConfig, public virtual BinaryCacheStore
 {
-    std::string ociRepo;
+    std::string ociName;
+    std::string ociEndpoint;
 
     OciBinaryCacheStore(
         const std::string & scheme,
-        const std::string & repo,
+        const std::string & name,
         const Params & params)
-        : ociRepo(repo)
+        : ociName(name)
     {
         diskCache = getNarInfoDiskCache();
+        std::string_view repo{ name };
+        auto registry = splitPrefixTo(repo, '/');
+        ociEndpoint = "https://" + registry + "/v2/" + repo + "/";
     }
 
     std::string getUri() override
     {
-        return "oci://" + ociRepo;
+        return "oci-bc://" + ociName;
     }
 
     void init() override
     {
+        if (auto cacheInfo = diskCache->upToDateCacheExists(getUri())) {
+            wantMassQuery.setDefault(cacheInfo->wantMassQuery);
+            priority.setDefault(cacheInfo->priority);
+        } else {
+            BinaryCacheStore::init();
+            diskCache->createCache(getUri(), storeDir, wantMassQuery, priority);
+        }
+    }
+
+    /**
+     * Use hashed path as OCI digest, this isn't actually a content digest.
+     * 
+     * OCI complient registries SHOULD allow digests with unrecognized
+     * algorithms to pass validation
+     */
+    std::string ociDigest(const std::string & path)
+    {
+        auto hash = hashString(htSHA256, path);
+        return "oci-bc:" + hash.to_string(Base32, false);
     }
 
     bool fileExists(const std::string & path) override
     {
-    }
-
-    void uploadFile(const std::string & path,
-        std::shared_ptr<std::basic_iostream<char>> istream,
-        const std::string & mimeType,
-        const std::string & contentEncoding)
-    {
+        auto digest = ociDigest(path);
+        // TODO: HEAD blobs/<digest>
     }
 
     void upsertFile(const std::string & path,
         std::shared_ptr<std::basic_iostream<char>> istream,
         const std::string & mimeType) override
     {
+        auto digest = ociDigest(path);
+        // TODO: upload blob, count size
+
+        auto manifest = nlohmann::json{
+            {"schemaVersion", 2},
+            {"config", {
+                {"mediaType", "application/vnd.oci.image.config.v1+json"},
+            }},
+            {"layers", {
+                {
+                    {"mediaType", mimeType},
+                    {"digest", digest},
+                    {"size", },
+                },
+            }}
+        };
+        // TODO: upload manifest
     }
 
     void getFile(const std::string & path, Sink & sink) override
     {
+        auto digest = ociDigest(path);
+        // TODO: GET blobs/<digest>
     }
 
     /**
@@ -73,6 +113,10 @@ struct OciBinaryCacheStore : public virtual OciBinaryCacheStoreConfig, public vi
         return std::nullopt;
     }
 
-    static std::set<std::string> uriSchemes() { return {"oci"}; }
+    /**
+     * Reserve the plain oci:// scheme for full-fledged
+     * store implementation
+     */
+    static std::set<std::string> uriSchemes() { return {"oci-bc"}; }
 
 }
--- src/libstore/oci-binary-cache-store.md
+++ src/libstore/oci-binary-cache-store.md
@@ -1,7 +1,10 @@
 R"(
 
-**Store URL format**: `oci://`*repo*
+**Store URL format**: `oci-bc://`*registry*`/`*repo*
 
 This store allows reading and writing a binary cache stored in an OCI registry.
+
+This implementation simply treats the OCI registry as a generic blob store
+through the `BinaryCacheStore` interface.
 
 )"
